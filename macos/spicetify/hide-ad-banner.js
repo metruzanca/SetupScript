eaa63fac8ef13aa52c144a981b58ce1c460ca611
metruzanca <metruzanca@gmail.com>
2021-01-11 10:16:15 +0100

    Moved spicetify to its own subfolder Improved the spicetify script

---
+++ macos/spicetify/hide-ad-banner.js
@@ -0,0 +1,3 @@
+// Just hides the visual banner, because I like my app to look clean.
+// Does not cause spotify to loose any revenue as the ad is not being blocked, just out of view.
+document.querySelector('#view-leaderboard-ad').style.display = 'none'
--- macos/spicetify/spicetify.sh
+++ macos/spicetify/spicetify.sh
@@ -13,7 +13,7 @@ https://github.com/morpheusthewhite/spicetify-themes.git
 cd spicetify-themes
 cp -r * ~/spicetify_data/Themes
 
-# Apply DribbblishDynamic-Dark
+# Apply DribbblishDynamic-Dark & install plugin
 spicetify config current_theme DribbblishDynamic color_scheme Dark             
 cd "$(dirname "$(spicetify -c)")/Themes/DribbblishDynamic"
 cp dribbblish-dynamic.js ../../Extensions
@@ -22,10 +22,17 @@ spicetify config current_theme DribbblishDynamic color_scheme dark
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
 
 # Apply base extensions
-spicetify config extensions dribbblish-dynamic.js
 spicetify config extensions bookmark.js
 spicetify config extensions fullAppDisplay.js
 spicetify config extensions newRelease.js
+
+# Install custom extensions
+# TODO this cp might not work. idk how cwd works in bash. I assume its relative to the running bash file?
+cp ./hide-ad-banner.js "$(dirname "$(spicetify -c)")/Extensions/"
+spicetify config extensions hide-ad-banner.js
+
+# TODO Potentially add an Extensions folder here to make this script more dynamic
+# Need to figure out how to do that in bash.
 
 # Apply all above changes
 spicetify apply
